4ac07e9946cf4469ea238962116f1643a55b11d4
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2024-06-25 13:20:04 +0530

    Counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let [counter,setcounter]=useState(15);
+  const AddValue = () =>{
+    counter=counter+1;
+    set(counter);
+  }
+  const RemoveValue = () =>{
+    counter=counter-1;
+    set(counter);
+  }
+  const MultiplyValue = () =>{
+    counter=counter*2;
+    set(counter);
+  }
+  const DivideValue = () =>{
+    counter=counter/2;
+    set(counter);
+  }
+
+  const set = (counter) =>{
+    setcounter(counter);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Counter: {counter}</p>
+        <div className="SetValue">
+        <button className='Edit' onClick={AddValue}>Add Value</button>
+        <button className='Edit' onClick={RemoveValue}>Subtract Value</button>
+        <button className='Edit' onClick={MultiplyValue}>Multiply Value by 2</button>
+        <button className='Edit' onClick={DivideValue}>Divide Value by 2</button>
+        </div>
       </header>
     </div>
   );
